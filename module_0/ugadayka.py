aaaa49dca96f39106c1f84fa63a3208d7f3a3c60
Asus <dimetry76@gmail.com>
2021-09-12 18:48:28 +0200

    SkillFactory

--- module_0/ugadayka.py
+++ module_0/ugadayka.py
@@ -7,14 +7,16 @@ predict_max = 100
 predict_min = 1
 
 while True:  # бесконечный цикл
-    # predict = int(input())  # предполагаемое число
-    predict = (predict_max + 1 - predict_min)/2
+    predict = predict_min+(predict_max - predict_min)//2
+    print(f'Пробуем {predict}')
     count += 1  # плюсуем попытку
     if number == predict:
         break  # выход из цикла, если угадали
     elif number > predict:
         print(f"Угадываемое число больше {predict} ")
+        predict_min = predict
     elif number < predict:
         print(f"Угадываемое число меньше {predict} ")
+        predict_max = predict
 
 print(f"Вы угадали число {number} за {count} попыток.")
