6959eb56271d8a4b2c025fffe3ce7185a3e51c82
Asus <dimetry76@gmail.com>
2021-09-12 18:30:35 +0200

    SkillFactory

--- module_0/ugadayka.py
+++ module_0/ugadayka.py
@@ -3,9 +3,12 @@ import numpy as np
 count = 0  # счетчик попыток
 number = np.random.randint(1, 101)  # загадали число
 print("Загадано число от 1 до 100")
+predict_max = 100
+predict_min = 1
 
 while True:  # бесконечный цикл
-    predict = int(input())  # предполагаемое число
+    # predict = int(input())  # предполагаемое число
+    predict = (predict_max + 1 - predict_min)/2
     count += 1  # плюсуем попытку
     if number == predict:
         break  # выход из цикла, если угадали
