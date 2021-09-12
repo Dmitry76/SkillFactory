86cadbdb8bb027f1e783fa90846ef1f60b5cacf1
Asus <dimetry76@gmail.com>
2021-09-12 22:04:04 +0200

    SkillFactory

--- module_0/ugadayka.py
+++ module_0/ugadayka.py
@@ -1,16 +1,21 @@
 import numpy as np
 
-
-def predict(pr_min, pr_max):
-    result = predict_min + (predict_max - predict_min) // 2  # расчитываем предполагаемое число
-    return result
-
+#  ПЕРЕМЕННЫЕ
 count = 0  # счетчик попыток
 number = np.random.randint(1, 101)  # загадали число
 print("Загадано число от 1 до 100")
 predict_max = 100  # максимум загаданного интервала
 predict_min = 1  # минимум загаданного интервала
 
+#  ФУНКЦИИ
+def predict(pr_min, pr_max):  #  РАСЧЕТ ПРЕДПОЛАГАЕМОГО ЧИСЛА
+    """Функция расчитывает подходящее предполагаемое число,
+    исходя из известных минимума и максимума интервала"""
+    result = predict_min + (predict_max - predict_min) // 2  # формула расчета предполагаемого числа
+    return result
+
+
+#  РАБОЧАЯ ОБЛАСТЬ
 while True:  # бесконечный цикл
     predict_number = int(predict(predict_min, predict_max))  # расчитываем предполагаемое число
     print(f'Пробуем {predict_number}')  # выводим предполагаемое число в консоль
