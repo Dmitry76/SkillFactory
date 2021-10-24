279a94f4c029bb379e5c294337b47a7a27421b2d
Asus2021 <007@cebb.info>
2021-10-24 13:59:26 +0200

    test

--- MySQL_query.py
+++ MySQL_query.py
@@ -116,8 +116,7 @@ def query_execute(host, user, password, database, my_query):
 # СТАРТУЕМ, ВВОД ХОСТА И ИМЯ БД
 host_adr = input("Host: ")
 host_user = input("Пользователь: ")
-host_pass = getpass.getpass()
-# host_pass = input("pass: ")
+host_pass = getpass.getpass("pass: ")
 if show_databases(host_adr, host_user, host_pass):
     bd_name = input("Имя БД: ")
 else:
