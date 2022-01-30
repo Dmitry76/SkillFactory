c6032bc56ee4dcaaf544cfb78597892dedd2fa76
Asus-007 <007@cebb.info>
2022-01-30 21:54:59 +0200

    Merge remote-tracking branch 'origin/master'

--- pandas/test_pandas.py
+++ pandas/test_pandas.py
@@ -1,19 +1,30 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+# ВЫБОР ИСТОЧНИКА ДАННЫХ
 football = pd.read_csv("data_sf.csv")
 
-print(football[football.Age > 35].Wage)  # выводим колонку Wage записей, где Age больше 20
-print(football[football.Age > football.Age.mean()])  # записи таблицы, где Age выше среднего
+# ОБЪЕДИНЕНИЕ ТАБЛИЦ
 
-# отдельные поля выборки верхние 5 записей
+
+# ВЫБОРКА СТОЛБЦОВ
 x = football[football.Nationality == 'Portugal'].loc[:, ['Name', 'Age']].head()
 print(x)
 
-#  ВЫВОД ГРАФИКОВ
-s = football[football.Value > football.Value.mean()].Nationality.value_counts().loc[lambda x1: x1 > 50]
+
+# ГРУППИРОВКА ДАННЫХ И ПРИМЕНЕНИЕ АНАЛИТИКИ
 r = football[football.Wage > football.Wage.mean()].Nationality.value_counts().loc[lambda x1: x1 > 50]
+s = football[football.Value > football.Value.mean()].Nationality.value_counts().loc[lambda x1: x1 > 50]
+print('===========')
+print(r, s)
+print('===========')
+print(football[football.Age > 35].Wage)  # выводим колонку Wage записей, где Age больше 20
+print(football[football.Age > football.Age.mean()])  # записи таблицы, где Age выше среднего
+
+# ОТРИСОВКА ГРАФОВ
 plt.plot(r)
 plt.plot(s)
 plt.savefig('mpl_out.png')  # сохраняем график
 plt.show()  # show graph
+
