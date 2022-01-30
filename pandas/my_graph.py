f4d64ea8046111f82cd052bbba4780cadf54189d
Asus-007 <007@cebb.info>
2022-01-30 15:09:53 +0200

    Merge remote-tracking branch 'origin/master'

--- pandas/my_graph.py
+++ pandas/my_graph.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 football = pd.read_csv("data_sf.csv")
-
+titanic = pd.read_csv('train.csv')
+stat_table = pd.read_excel('client.xlsx', sheet_name='stat')
 
 # ФУНКЦИЯ СОЗДАНИЯ ГРАФА СВЯЗЕЙ ОБЪЕКТОВ
 def graph_links(start):
@@ -23,8 +24,9 @@ def graph_links(start):
 def graph_daily_statistic(start):
     if start == 1:
         plt.title('Статистика заполнения таблицы')
-        days = ['26.01', '27.01', '28.01']
-        records = [0, 0, 9]
+        print(stat_table)
+        days = stat_table['date'].dt.strftime('%d-%m')  # Меняем формат вывода даты
+        records = stat_table['kol']
         plt.plot(days, records)
     else:
         return
@@ -41,10 +43,20 @@ def graph_football(start):
         return
 
 
-# ФУНКЦИЯ ЭКСПЕРИМЕНТОВ С PANDAS
+# ФУНКЦИЯ ЭКСПЕРИМЕНТОВ С PANDAS football
 def football_anal(start):
     if start == 1:
         result = football[['Name', 'Age']].head(10).max()
+        return result
+    else:
+        result = ''
+        return result
+
+
+# ФУНКЦИЯ ЭКСПЕРИМЕНТОВ С PANDAS Titanic
+def titanic_anal(start):
+    if start == 1:
+        result = titanic[['Name', 'Sex', 'Age']]
         return result
     else:
         result = ''
@@ -62,12 +74,13 @@ def run_plt(save, show):
 
 
 # ВЫВОД РЕЗУЛЬТАТА ЭКСПЕРИМЕНТОВ С PANDAS
-print(football_anal(1))     # ВЫВОДИМ РЕЗУЛЬТАТЫ В КОНСОЛЬ (1) ИЛИ (0) ЕСЛИ НЕ ВЫВОДИМ
+print(football_anal(0))     # ВЫВОДИМ РЕЗУЛЬТАТЫ В КОНСОЛЬ (1) ИЛИ (0) ЕСЛИ НЕ ВЫВОДИМ
+print(titanic_anal(0))      # ВЫВОДИМ РЕЗУЛЬТАТЫ В КОНСОЛЬ (1) ИЛИ (0) ЕСЛИ НЕ ВЫВОДИМ
 
 # ВЫБОР ГРАФА И ВЫВОД ЕГО НА ЭКРАН C СОХРАНЕНИЕМ В ФАЙЛ
 
 graph_links(0)              # ВЫБОР ГРАФА СВЯЗЕЙ (1) - ДА, (0) - НЕТ
-graph_daily_statistic(0)    # ВЫБОР ГРАФА ЕЖЕДНЕВНЫХ ЗНАЧЕНИЙ (1) - ДА, (0) - НЕТ
+graph_daily_statistic(1)    # ВЫБОР ГРАФА ЕЖЕДНЕВНЫХ ЗНАЧЕНИЙ (1) - ДА, (0) - НЕТ
 graph_football(0)           # ВЫБОР ГРАФА ФУТБОЛЬНОЙ СТАТИСТИКИ (1) - ДА, (0) - НЕТ
-run_plt(0, 0)               # СОХРАНЕНИЕ И ОТРИСОВКА ГРАФА (1) - ДА, (0) - НЕТ
+run_plt(0, 1)               # СОХРАНЕНИЕ И ОТРИСОВКА ГРАФА (1) - ДА, (0) - НЕТ
 
